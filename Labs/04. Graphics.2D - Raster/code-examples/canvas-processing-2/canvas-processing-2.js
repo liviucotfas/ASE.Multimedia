58a622158d57884030191496fd88c93d88cc9b94
Cotfas Liviu-Adrian <lcotfas@gmail.com>
2017-11-28 20:03:39 +0200

    Updated sample

--- Labs/04. Graphics.2D - Raster/code-examples/canvas-processing-2/canvas-processing-2.js
+++ Labs/04. Graphics.2D - Raster/code-examples/canvas-processing-2/canvas-processing-2.js
@@ -9,7 +9,19 @@ Hint: use the empty() and append() jQuery methods
 3. Open the Developer Tools in the browser of your choice. Choose the Console tab. Click the browse button in the application and afterwards choose the Cancel option. An error will be displayed in the console tab. Try to fix it.
 */
 
-function loadFile(file) {
+$(function(){
+    app.canvasImage = document.getElementById("result");
+    app.canvasHistogram = document.getElementById("histogram");
+    app.histogram = new Histogram(app.canvasHistogram);
+});
+
+var app={
+    canvasImage:null,
+    canvasHistogram:null,
+    histogram:null,
+}
+
+app.loadFile = function(file) {
     //1. create the reader
     var reader = new FileReader();
     //2. attach events
@@ -17,12 +29,10 @@ function loadFile(file) {
         //1. create the image element
         var img = new Image();
         //2. attach events
-        img.onload = function(){
-            var canvasImage = document.getElementById("result");
-            displayImageOnCanvas(canvasImage, img);
-            var canvasHistogram = document.getElementById("histogram");
-            computeAndDrawHistogram(canvasImage, canvasHistogram);
-            convertToGreyScale(canvasImage);
+        img.onload = function(e){
+            app.displayImageOnCanvas(e.target);
+            app.drawHistogram();            
+            app.convertToGreyScale();
         };
         img.onerror = function (msg, source, lineNo) {
             alert("Mesaj eroare: {0}".format(msg));
@@ -31,21 +41,15 @@ function loadFile(file) {
         img.src = event.target.result;
     }
     //3. start loading the file
-    reader.readAsDataURL(file);  
-
-      //jQuery Equivalent: 
-    // $(document.createElement('img')).get(0)
-    // $("<img>").get(0)
-    // //https://learn.jquery.com/using-jquery-core/faq/how-do-i-pull-a-native-dom-element-from-a-jquery-object/
-
+    reader.readAsDataURL(file);
 }
 
-function displayImageOnCanvas(canvas, img)
+app.displayImageOnCanvas = function(img)
 {
-    canvas.height = img.height;
-    canvas.width = img.width;
+    app.canvasImage.height = img.height;
+    app.canvasImage.width = img.width;
 
-    var context = canvas.getContext("2d");
+    var context =  app.canvasImage.getContext("2d");
     context.drawImage(img, 0, 0);
 
     context.font = "bold 10pt sans-serif";
@@ -54,91 +58,14 @@ function displayImageOnCanvas(canvas, img)
     context.fillText(img.src, 8, 25);
 }
 
-function computeAndDrawHistogram(canvasImage, canvasHistogram)
-{ 
-    var vR = new Array(); //equivalent to var vR = []; Futher reading: http://www.w3schools.com/js/js_arrays.asp
-    var vG = [];
-    var vB = [];
-    for (var i = 0; i < 256; i++){ 
-        vR[i] = 0; vG[i] = 0; vB[i] = 0; 
-    }
-
-    drawingLibrary.processImage(canvasImage,
-        function (x, y, r, g, b, a) {
-            vR[r]++; vG[g]++; vB[b]++;
-        });
-
-    drawingLibrary.drawHistogram(canvasHistogram, vR, vG, vB);
+app.drawHistogram = function(){
+    let result = drawingLibrary.analyzeColorChannels(app.canvasImage);
+    app.histogram.draw(result.vR, result.vG, result.vB);
 }
 
-function convertToGreyScale(canvasImage)
-{
-    //TODO
+app.convertToGreyScale = function(){
+    drawingLibrary.convertToGreyScale(app.canvasImage);
 }
-
- var drawingLibrary={
-    processImage : function(canvas, action) {
-        var context = canvas.getContext("2d");
-    
-        var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-        var data = imageData.data;
-    
-        // iterate over all pixels based on x and y coordinates.
-    
-        // loop through each row
-        for (var y = 0; y < canvas.height; y++) {
-            // loop through each column
-            for (var x = 0; x < canvas.width; x++) {
-                var offset = ((canvas.width * y) + x) * 4;
-    
-                var red = data[offset];
-                var green = data[offset + 1];
-                var blue = data[offset + 2];
-                var alpha = data[offset + 3];
-    
-                var result = action(x, y, red, green, blue, alpha);
-    
-                if (typeof (result) != "undefined" && result instanceof Array) {
-                    for (var i = 0; i < result.length; i++) {
-                        data[offset + i] = result[i];
-                    }
-                }
-            }
-        }
-    
-        // draw the new image
-        context.putImageData(imageData, 0, 0);
-    },
-    drawHistogram : function (canvas, vR, vG, vB) {
-        var h = canvas.height;
-        var w = canvas.width / vR.length;
-    
-        var context = canvas.getContext("2d");
-        context.fillStyle = "#DEDEDE";
-        context.fillRect(0, 0, canvas.width, canvas.height);
-    
-        var f = canvas.height * 0.9 / Math.max.apply(Math, [].concat(vR, vG, vB));
-    
-        for (var i = 0; i < vR.length; i++) {
-    
-            context.fillStyle = "rgba(10%,10%,10%,0.2)";
-            //rgba(red, green, blue, alpha) 
-            //The alpha parameter is a number between 0.0 (fully transparent) and 1.0 (fully opaque).
-            context.fillRect(i * w, h - (vR[i] + vG[i] + vB[i]) * f, w, (vR[i] + vG[i] + vB[i]) * f);
-    
-            context.fillStyle = "rgba(100%,0%,0%,0.3)";
-            context.fillRect(i * w, h - vR[i] * f, w, vR[i] * f);
-    
-            context.fillStyle = "rgba(0%,100%,0%,0.3)";
-            context.fillRect(i * w, h - vG[i] * f, w, vG[i] * f);
-    
-            context.fillStyle = "rgba(0%,0%,100%,0.3)";
-            context.fillRect(i * w, h - vB[i] * f, w, vB[i] * f);
-        }
-    }
-}
-
-
 
 //extends the string prototype object
 //Inheritance and the prototype chain: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain 
