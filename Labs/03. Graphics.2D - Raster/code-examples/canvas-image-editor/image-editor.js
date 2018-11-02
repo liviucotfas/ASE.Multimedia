c722fc53738edb1827ef8863c167c7c438b64246
Cotfas Liviu-Adrian <lcotfas@gmail.com>
2018-11-02 22:16:19 +0200

    Update Labs/03. Graphics.2D - Raster/code-examples/canvas-image-editor/image-editor.js

--- Labs/03. Graphics.2D - Raster/code-examples/canvas-image-editor/image-editor.js
+++ Labs/03. Graphics.2D - Raster/code-examples/canvas-image-editor/image-editor.js
@@ -16,6 +16,7 @@ Hint: r'=0; g'=255; b'=0;
 Hint: r'=0; g'=0; b'=b;
 7. Implement 2Channels
 Hint: check https://gist.github.com/anonymous/1888841
+8. Only display the download link when an effect is applied
 8. (optional) Use Bootstrap for the UI (https://getbootstrap.com/)
 9. (optional) Implement pixelate
 Hint: check https://gist.github.com/anonymous/1888841
