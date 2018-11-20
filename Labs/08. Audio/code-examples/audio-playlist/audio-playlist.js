4a830e62e1ea0b1313ee03d038c9a4fb35501c76
Cotfas Liviu-Adrian <lcotfas@gmail.com>
2018-11-20 22:19:32 +0200

    Removed jQuery

--- Labs/08. Audio/code-examples/audio-playlist/audio-playlist.js
+++ Labs/08. Audio/code-examples/audio-playlist/audio-playlist.js
@@ -7,9 +7,13 @@ var app={
 
 // 3. Play/Next methods
 app.play = function(url) {
-    $("#playlist li[data-url='" + url + "']")
-        .addClass("active")
-        .siblings().removeClass("active");
+    let elements = document.querySelectorAll("#playlist li.active");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].classList.remove('active');
+     }
+    
+    let selectedElement = document.querySelector("#playlist li[data-url='" + url + "']");
+    selectedElement.classList.add('active');
 
     app.audio.src = url;
     app.audio.load();
@@ -25,51 +29,53 @@ app.next = function() {
     app.play(app.tracks[index]);
 }
 
-$(function () {
+app.load = function(){
     app.audio = document.getElementById('audio');
     app.tracks = []; //track list
 
     // 1. Display the current time in seconds
     setInterval(function () {
+
+        let element = document.querySelector("#controls span");
+
         if (app.audio.duration) {
-            $("#controls span").text(
-              Math.floor(app.audio.currentTime) + " / " +
-                Math.floor(app.audio.duration));
+            element.textContent = Math.floor(app.audio.currentTime) + " / " + Math.floor(app.audio.duration);
         }
         else {
-            $("#controls span").text("Loading...");
+            element.textContent = "...";
         };
     }, 30);
 
     // 2. Iterate over the playlist in order to associate events
-    $("#playlist li").each(function () {
-        var url = $(this).data("url");
+    let elements = document.querySelectorAll("#playlist li");
+    for(let i = 0; i<elements.length; i++){
+
+        var url = elements[i].dataset.url;
         app.tracks.push(url);
 
-        $(this).click(function () {
-            app.play(url);
+        elements[i].addEventListener('click', function () {
+            app.play(this.dataset.url);
         });
-    });
+    }
 
     // 4. Associate events
-    $("#btnPlayPause").click(function () {
+    document.getElementById("btnPlayPause").addEventListener("click", function () {
         if (app.audio.paused) {
             app.audio.play();
-            $("#btnPlayPause").text("||");
+            this.textContent = "||";
         }
         else {
             app.audio.pause();
-            $("#btnPlayPause").text(">");
+            this.textContent = ">";
         }
     });
 
-    $("#btnForward").click(function () {
+    document.getElementById("btnForward")
+        .addEventListener('click', function () {
         app.audio.currentTime += 10;
     });
 
-    $("#btnNext").click(app.next);
+    document.getElementById("btnNext").addEventListener('click', app.next);
 
-    $(app.audio).on("ended", app.next);
-
-    app.next(); // start playing
-});
+    app.audio.addEventListener("ended", app.next);
+};
